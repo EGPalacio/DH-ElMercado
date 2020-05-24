5c3d19e9771ec579dbbe7da6d2e714014bffe15a
Leandro797 <lah797@gmail.com>
2020-05-24 05:09:24 -0300

    Detalle de Productos hasta el json - 24/05

--- myapps/controllers/detalleProductosControllers.js
+++ myapps/controllers/detalleProductosControllers.js
@@ -1,9 +1,19 @@
 let arrayProductos = require('../articulosJS');
 
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 let productoControllers = {
     productos: (req,res) => {
         let index = arrayProductos;
-        res.render('detalleProductos', {"index":index});
+        let pdtoID = req.params.id;
+        let productFind = arrayProductos.find(pdto => pdto.id == pdtoID);
+        console.log(productFind);
+
+        res.render('detalleProductos', {
+            "index":index,
+            productFind,
+			thousandGenerator: toThousand
+        });
     }
 };
 
