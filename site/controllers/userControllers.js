8a2decede622cf6d6f6c01842959b2e8446111e2
EGPalacio <gastonpalacio@hotmail.com>
2020-06-10 01:03:23 -0300

    ajuste login para redirección al home

--- site/controllers/userControllers.js
+++ site/controllers/userControllers.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const bcrypt = require("bcrypt"); /* Después borrar lo agregué para probar mi parte del código */
-var {check, validationResult, body} = require("express-validator");
+var { check, validationResult, body } = require("express-validator");
 
 const usersFilePath = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
@@ -17,94 +17,96 @@ const newUsertId = newId.newUserId;
 //const accounts = JSON.parse(fs.readFileSync(accountsFilePath, 'utf-8'));
 
 let userControllers = {
-    login: (req,res) => {
+    login: (req, res) => {
         res.render('login');
-	},
-	loggedIn: (req,res) => {
-		let errors = validationResult(req);
-		if (errors.isEmpty()){
-			let usuarioALoguearse;
+    },
+    loggedIn: (req, res) => {
+        let errors = validationResult(req);
+        if (errors.isEmpty()) {
+            let usuarioALoguearse;
 
-			for ( let user of users){
-				if (user.email == req.body.email && bcrypt.compareSync(req.body.password, user.password)){
-				   usuarioALoguearse = user;
-				}
-			}
+            for (let user of users) {
+                if (user.email == req.body.email && bcrypt.compareSync(req.body.password, user.password)) {
+                    usuarioALoguearse = user;
+                }
+            }
 
-			if (usuarioALoguearse == undefined){
-				return res.render("login", {errors: [{msg: "Credenciales no válidas"}]})
-			}
-                 /* Acá está el session */
-			req.session.usuarioLogueado = usuarioALoguearse;
+            if (usuarioALoguearse == undefined) {
+                return res.render("login", { errors: [{ msg: "Credenciales no válidas" }] })
+            }
+            /* Acá está el session */
+            req.session.usuarioLogueado = usuarioALoguearse;
 
-			     /* Acá está la cookie */
-			if (req.body.recordame != undefined){
-				 res.cookie("recordame", usuarioALoguearse, {maxAge: 60000})
-		  }
+            /* Acá está la cookie */
+            if (req.body.recordame != undefined) {
+                res.cookie("recordame", usuarioALoguearse, { maxAge: 60000 })
+            }
 
-			res.send(req.session.usuarioLogueado) /* esto hay que cambiarlo por un redirect al home y que en vez de "login" en el header se lea el "firstName" */
-
-		}else{
-			res.render("login", {errors: errors.errors});
-		}
-	},
-    register : (req, res) => {
+            console.log(` ==> confirmación de usuario logueado como: ${req.session.usuarioLogueado.firstName}`);
+            /* esto hay que cambiarlo por un redirect al home y que en vez de "login" en el header se lea el "firstName" */
+            // EGP ajusté el código para redireccionarlo al home y dejar por terminal la confirmación del login
+            res.redirect('/')
+        } else {
+            res.render("login", { errors: errors.errors });
+        }
+    },
+    register: (req, res) => {
         res.render('register');
     },
-    store : (req, res) => {
-		
-		console.log(req.files[0]);
+    store: (req, res) => {
 
-		let errors = validationResult(req);
-		
-		
-		
-		
-		
-		if (errors.isEmpty())
-		
-		{
+        console.log(req.files[0]);
 
-			let avatar
-
-			if (req.files[0] == undefined ) {
-			avatar ='';
-			} else {
-			avatar = req.files[0].filename;
-			};
-		
-			// Do the magic
-			let usuario = {
-			id : newUsertId,
-			...req.body,
-			password: bcrypt.hashSync(req.body.password, 10),
-			password_repeat: "",  
-			avatar : avatar,
-			}
-			//leer el json
-			//let rutaJson = path.join(__dirname, '../data/accountsDataBase.json');
-			let archivoUsuario = fs.readFileSync('./data/users.json', {encoding: 'utf-8'});
-
-			let usuarios;
-
-			if(archivoUsuario == "") {
-			usuarios = [ ];
-			} else {
-			usuarios = JSON.parse(archivoUsuario);
-			}
-		
-			usuarios.push(usuario);
+        let errors = validationResult(req);
 
 
-			usuariosJSON = JSON.stringify(usuarios, null, ' ');
-			fs.writeFileSync('./data/users.json', usuariosJSON);
 
-			res.redirect('/login');
 
-		} else{
-			res.render("register", {errors: errors.errors});
-		}
-        
+
+        if (errors.isEmpty())
+
+        {
+
+            let avatar
+
+            if (req.files[0] == undefined) {
+                avatar = '';
+            } else {
+                avatar = req.files[0].filename;
+            };
+
+            // Do the magic
+            let usuario = {
+                    id: newUsertId,
+                    ...req.body,
+                    password: bcrypt.hashSync(req.body.password, 10),
+                    password_repeat: "",
+                    avatar: avatar,
+                }
+                //leer el json
+                //let rutaJson = path.join(__dirname, '../data/accountsDataBase.json');
+            let archivoUsuario = fs.readFileSync('./data/users.json', { encoding: 'utf-8' });
+
+            let usuarios;
+
+            if (archivoUsuario == "") {
+                usuarios = [];
+            } else {
+                usuarios = JSON.parse(archivoUsuario);
+            }
+
+            usuarios.push(usuario);
+
+
+            usuariosJSON = JSON.stringify(usuarios, null, ' ');
+            fs.writeFileSync('./data/users.json', usuariosJSON);
+
+            res.redirect('/login');
+
+        } else {
+            res.render("register", { errors: errors.errors });
+        }
+
     },
 };
 
