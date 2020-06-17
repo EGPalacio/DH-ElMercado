e85189da54916e68fd46341b60ab307627635ed7
martuns <martuns@gmail.com>
2020-06-16 23:54:15 -0300

    Upgrade Front-End

--- site/articulosJS.js
+++ site/articulosJS.js
@@ -1,6 +1,6 @@
 let fs = require('fs');
 let path = require('path');
 
-let arrayArticulos = JSON.parse(fs.readFileSync(path.join(__dirname, './data/productsDatabase.json'), 'utf-8'));
+let arrayArticulos = JSON.parse(fs.readFileSync(path.join(__dirname, './data/productsDataBase.json'), 'utf-8'));
 
 module.exports = arrayArticulos;
