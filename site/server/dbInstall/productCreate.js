619bcefe5cc28acdb95b53e391a838cf93f5984b
EGPalacio <gastonpalacio@hotmail.com>
2020-06-24 02:08:41 -0300

    corr bug productCreate sequence

--- site/server/dbInstall/productCreate.js
+++ site/server/dbInstall/productCreate.js
@@ -44,34 +44,6 @@ let discoToCreate = [{
 
 console.log(discoToCreate);
 
-// Alter table CATEGORIES- adjustments to existing model
-db.Category.sync({ alter: true })
-    .then((categTable) => {
-        console.log(`    => the table Category has just created!`);
-
-        // Product DB upload
-        db.Category.bulkCreate(categToCreate)
-            .then((result) => {
-                console.log('    => Categorías creados en DB')
-            })
-            .catch((err) => { console.log(err) });
-    })
-    .catch((err) => { console.log(err) });
-
-// Alter table DISCOUNT- adjustments to existing model
-db.Discount.sync({ alter: true })
-    .then((discTable) => {
-        console.log(`    => the table Discount has just created!`);
-
-        // Product DB upload
-        db.Discount.bulkCreate(discoToCreate)
-            .then((result) => {
-                console.log('    => Discounts creados en DB')
-            })
-            .catch((err) => { console.log(err) });
-    })
-    .catch((err) => { console.log(err) });
-
 // Array construction for DB upload in bulk create
 let prodToCreate = [];
 products.products.forEach(producto => {
@@ -85,28 +57,65 @@ products.products.forEach(producto => {
 
 console.log(prodToCreate);
 
-// Alter table PRODUCTS - adjustments to existing model
-db.Product.sync({ alter: true })
-    .then((prodTable) => {
-        console.log(`the table Products has just created!`);
+// Alter table CATEGORIES- adjustments to existing model
+db.Category.sync({ alter: true })
+    .then((categTable) => {
+        console.log(`    => the table Category has just created!`);
 
         // Product DB upload
-        db.Product.bulkCreate(prodToCreate)
+        db.Category.bulkCreate(categToCreate)
             .then((result) => {
-                console.log('Productos creados en DB');
+                console.log('    => Categorías creados en DB')
 
-                // Product Elim Null items
-                db.Product.destroy({
-                        where: {
-                            name: 'Eliminar'
-                        },
+                // Alter table DISCOUNT- adjustments to existing model
+                db.Discount.sync({ alter: true })
+                    .then((discTable) => {
+                        console.log(`    => the table Discount has just created!`);
+
+                        // Product DB upload
+                        db.Discount.bulkCreate(discoToCreate)
+                            .then((result) => {
+                                console.log('    => Discounts creados en DB')
+
+                                // Alter table PRODUCTS - adjustments to existing model
+                                db.Product.sync({ alter: true })
+                                    .then((prodTable) => {
+                                        console.log(`the table Products has just created!`);
+
+                                        // Product DB upload
+                                        db.Product.bulkCreate(prodToCreate)
+                                            .then((result) => {
+                                                console.log('Productos creados en DB');
+
+                                                // Product Elim Null items
+                                                db.Product.destroy({
+                                                        where: {
+                                                            name: 'Eliminar'
+                                                        },
+                                                    })
+                                                    .then((distroyResult) => {
+                                                        console.log(`destroy completed - product DB clean`);
+                                                        console.log(distroyResult);
+                                                    })
+                                                    // Catch distroy null products
+                                                    .catch((err) => { console.log(err) });
+                                            })
+                                            // Catch bulkCreate Products
+                                            .catch((err) => { console.log(err) });
+                                    })
+                                    // Catch alterTable Products
+                                    .catch((err) => { console.log(err) });
+
+                            })
+                            // Catch bulkCreate Discounts
+                            .catch((err) => { console.log(err) });
                     })
-                    .then((distroyResult) => {
-                        console.log(`destroy completed - product DB clean`);
-                        console.log(distroyResult);
-                    })
+                    // Catch alterTable Discounts
                     .catch((err) => { console.log(err) });
+
             })
+            // Catch bulkCreate Categories
             .catch((err) => { console.log(err) });
     })
+    // Catch alterTables Categories Categories
     .catch((err) => { console.log(err) });
