155232826a73442d0fbe6c0ae91301a802cb6268
EGPalacio <gastonpalacio@hotmail.com>
2020-06-28 00:25:08 -0300

    ajuste bug log por terminal cambio campo firstname

--- site/controllers/profileController.js
+++ site/controllers/profileController.js
@@ -2,15 +2,17 @@ module.exports = {
     profile: (req, res) => {
         var user = req.session.usuarioLogueado
         if (user) {
-            let userName = req.session.usuarioLogueado.firstName
+            let userName = user.first_name
             console.log(` ==> acceso autorizado para: ${userName}`);
+            res.render('profile', {
+                tag: 'profile',
+                user: user
+            });
+
         } else {
             console.log(` ==> login requerido - redireccionado para confirmar acceso`);
             res.redirect('/login')
         }
-        res.render('profile', {
-            title: 'profile',
-            user: user
-        })
+
     }
 }
