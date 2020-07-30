7067ba8fa81ab4d4e4daad06da9b829d0304edc9
unknown <gastonpalacio@hotmail.com>
2020-07-30 00:40:32 -0300

    routes api product detail & product categories

--- site/routes/api/products.js
+++ site/routes/api/products.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 var productsAPIController = require("../../controllers/api/productsController");
 
-
 /* GET actors. */
 router.get("/products/", productsAPIController.list);
-
+router.get("/products/detail/:id", productsAPIController.prodDetail);
+router.get("/products/categories", productsAPIController.prodCateg);
 
 module.exports = router;
