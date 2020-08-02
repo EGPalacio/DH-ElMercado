77b3aefbd90da79f42eb178c017a78d459700c3b
unknown <gastonpalacio@hotmail.com>
2020-08-02 17:25:34 -0300

    widget categories completo

--- Dashboard/src/components/ContentRowTop.js
+++ Dashboard/src/components/ContentRowTop.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import ProductsInDB from './ProductsInDB';
 import AmountOfUsersInDB from './AmountOfUsersInDB';
 import Widget from './widgets/TotalCard';
 
+const api = axios.create({
+    method: 'GET',
+    baseURL: `http://localhost:3030/api`,
+});
+
 export default class ContentRowTop extends Component {
+    constructor (props){
+        super(props);
+        this.state = {
+            categCount: 'Cargando'
+        }
+    }
+
+    componentDidMount(){
+       api('/products/categories').then( (result) => {
+                    console.log(result.data.categCount);
+                    this.setState({categCount: result.data.categCount})
+                })
+    }
+
     render() {
         return (
             <div className="row">
@@ -11,7 +31,12 @@ export default class ContentRowTop extends Component {
                 <ProductsInDB />
 
             {/* <!-- Categories in DB --> */}
-                <Widget />
+                <Widget 
+                    title="Categorias del sitio"
+                    total={this.state.categCount}
+                    icon="fa-th-list"
+                    colorCard="secondary"
+                    />
 
             {/* <!-- Amount of users in DB --> */}
                 <AmountOfUsersInDB />
