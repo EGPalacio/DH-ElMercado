04c01e7d037935bc0145cbb6364833e2fb56bed2
unknown <gastonpalacio@hotmail.com>
2020-08-02 18:59:21 -0300

    component category card

---
+++ Dashboard/src/components/categories/CategoryCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class CategoryCard extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="col-lg-6 mb-4">
+                <div className="card bg-primary text-white shadow">
+                    <div className="card-body">
+                    <div className="col mr-2">
+                            <div className={`text-xs font-weight-bold text-white text-uppercase mb-1`}> {this.props.nombre}</div>
+                        </div>
+                        <div className="row col-auto">
+                            <div className="col-11 h5 mb-0 text-white-800">{this.props.importe}</div>
+                            <div className="h5 mb-0 text-white-800">{this.props.items}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
