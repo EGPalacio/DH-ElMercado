e6bd534616e095ee03aabeecc49260e31ad3527a
unknown <gastonpalacio@hotmail.com>
2020-08-02 17:15:54 -0300

    widget component using props

--- Dashboard/src/components/widgets/TotalCard.js
+++ Dashboard/src/components/widgets/TotalCard.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 
 export default class TotalCard extends Component {
+    constructor(props){
+        super(props);
+        console.log(this.props);
+        this.state ={
+            total: props.total,
+        }
+    }
     render() {
         return (
                 <div className="col-md-4 mb-4">
-                    <div className="card border-left-success shadow h-100 py-2">
+                    <div className={`card border-left-${this.props.colorCard} shadow h-100 py-2`}>
                         <div className="card-body">
                             <div className="row no-gutters align-items-center">
                                 <div className="col mr-2">
-                                    <div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Amount in products</div>
-                                    <div className="h5 mb-0 font-weight-bold text-gray-800">$546.456</div>
+                                    <div className={`text-xs font-weight-bold text-${this.props.colorCard} text-uppercase mb-1`}> {this.props.title}</div>
+                                    <div className="h5 mb-0 font-weight-bold text-gray-800">{this.props.total}</div>
                                 </div>
                                 <div className="col-auto">
-                                    <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                    <i className={`fas ${this.props.icon} fa-2x text-gray-300`}></i>
                                 </div>
                             </div>
                         </div>
